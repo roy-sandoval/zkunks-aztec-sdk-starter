f4b8397419c24fc04aee839afaf8d9067013d577
roy-sandoval <roy@roysandoval.com>
2022-10-09 07:23:01 -0500

    init

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -1,10 +1,11 @@
-import type { NextPage } from "next";
-import { ethers } from "ethers";
-import { JsonRpcSigner, Web3Provider } from "@ethersproject/providers";
-import { useEffect, useState } from "react";
+import type { NextPage } from 'next';
+import { ethers } from 'ethers';
+import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
+import { useEffect, useState } from 'react';
 import {
   AccountId,
   AztecSdk,
+  AssetValue,
   createAztecSdk,
   EthersAdapter,
   EthereumProvider,
@@ -14,14 +15,40 @@ import {
   SchnorrSigner,
   EthAddress,
   TxSettlementTime,
-} from "@aztec/sdk";
+  virtualAssetIdPlaceholder,
+  BridgeCallData,
+  DefiSettlementTime,
+} from '@aztec/sdk';
 
-import { randomBytes } from "crypto";
+import { randomBytes } from 'crypto';
 
-import {
-  depositEthToAztec,
-  registerAccount,
-} from "./utils";
+import { depositEthToAztec, registerAccount } from './utils';
+
+import Confetti from 'react-confetti';
+import Button from '../components/Global/Button';
+import Image from 'next/image';
+
+const bridgeReadAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+    name: 'owners',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+  },
+];
 
 const Home: NextPage = () => {
   const [isConnected, setIsConnected] = useState(false);
@@ -34,28 +61,37 @@ const Home: NextPage = () => {
   const [sdk, setSdk] = useState<null | AztecSdk>(null);
   const [account0, setAccount0] = useState<AztecSdkUser | null>(null);
   const [userExists, setUserExists] = useState<boolean>(false);
-  const [accountPrivateKey, setAccountPrivateKey] = useState<Buffer | null>(null);
-  const [accountPublicKey, setAccountPublicKey] = useState<GrumpkinAddress | null>(null);
-  const [spendingSigner, setSpendingSigner] = useState<SchnorrSigner | undefined>(undefined);
-  const [alias, setAlias] = useState("");
+  const [accountPrivateKey, setAccountPrivateKey] = useState<Buffer | null>(
+    null
+  );
+  const [accountPublicKey, setAccountPublicKey] =
+    useState<GrumpkinAddress | null>(null);
+  const [spendingSigner, setSpendingSigner] = useState<
+    SchnorrSigner | undefined
+  >(undefined);
+  const [alias, setAlias] = useState('');
   const [amount, setAmount] = useState(0);
+  const [virtualAssetId, setVirtualAssetId] = useState(0);
+  const [activeImg, setActiveImg] = useState(1);
+  const [paused, setPaused] = useState(false);
+  const [intervalSpeed, setIntervalSpeed] = useState(1000);
+  const [minted, setMinted] = useState(false);
+  const [quanity, setQuanity] = useState(499);
 
   useEffect(() => {
-    if (typeof window.ethereum !== "undefined") {
+    if (typeof window.ethereum !== 'undefined') {
       setHasMetamask(true);
     }
-    window.ethereum.on("accountsChanged", () => location.reload());
+    window.ethereum.on('accountsChanged', () => location.reload());
   });
-
   async function connect() {
     setConnecting(true);
-    if (typeof window.ethereum !== "undefined") {
+    if (typeof window.ethereum !== 'undefined') {
       try {
         let accounts = await ethereum.request({
-          method: "eth_requestAccounts",
+          method: 'eth_requestAccounts',
         });
         setEthAccount(EthAddress.fromString(accounts[0]));
-
 
         const ethersProvider: Web3Provider = new ethers.providers.Web3Provider(
           window.ethereum
@@ -65,17 +101,17 @@ const Home: NextPage = () => {
         );
 
         const sdk = await createAztecSdk(ethereumProvider, {
-          serverUrl: "https://api.aztec.network/aztec-connect-testnet/falafel", // goerli testnet
+          serverUrl: 'https://api.aztec.network/aztec-connect-testnet/falafel', // goerli testnet
           pollInterval: 1000,
           memoryDb: true,
-          debug: "bb:*",
+          debug: 'bb:*',
           flavour: SdkFlavour.PLAIN,
           minConfirmation: 1, // ETH block confirmations
         });
 
         await sdk.run();
 
-        console.log("Aztec SDK initialized", sdk);
+        console.log('Aztec SDK initialized', sdk);
         setIsConnected(true);
         setSigner(ethersProvider.getSigner());
         setEthereumProvider(ethereumProvider);
@@ -90,165 +126,315 @@ const Home: NextPage = () => {
   }
 
   async function login() {
-    const { publicKey: pubkey, privateKey } = await sdk!.generateAccountKeyPair(ethAccount!)
-    console.log("privacy key", privateKey);
-    console.log("public key", pubkey.toString());
+    const { publicKey: pubkey, privateKey } = await sdk!.generateAccountKeyPair(
+      ethAccount!
+    );
+    console.log('privacy key', privateKey);
+    console.log('public key', pubkey.toString());
 
     setAccountPrivateKey(privateKey);
     setAccountPublicKey(pubkey);
   }
 
   async function initUsersAndPrintBalances() {
-
     let account0 = (await sdk!.userExists(accountPublicKey!))
       ? await sdk!.getUser(accountPublicKey!)
       : await sdk!.addUser(accountPrivateKey!);
 
     setAccount0(account0!);
 
-    if ((await sdk?.isAccountRegistered(accountPublicKey!)))
-      setUserExists(true);
+    console.log(account0);
+
+    if (await sdk?.isAccountRegistered(accountPublicKey!)) setUserExists(true);
 
     await account0.awaitSynchronised();
     // Wait for the SDK to read & decrypt notes to get the latest balances
     console.log(
-      "zkETH balance",
+      'zkETH balance',
       sdk!.fromBaseUnits(
-        await sdk!.getBalance(account0.id, sdk!.getAssetIdBySymbol("ETH"))
+        await sdk!.getBalance(account0.id, sdk!.getAssetIdBySymbol('ETH'))
       )
     );
+  }
+
+  async function mint() {
+    const nftBridge = new BridgeCallData(
+      32,
+      0,
+      virtualAssetIdPlaceholder,
+      undefined,
+      undefined,
+      0
+    );
+
+    let bridge = nftBridge;
+
+    const tokenAssetValue: AssetValue = {
+      assetId: 0,
+      value: 1n,
+    };
+
+    const fee = (await sdk.getDefiFees(bridge))[DefiSettlementTime.INSTANT];
+    console.log(
+      'create defi controller',
+      accountPublicKey,
+      spendingSigner,
+      bridge,
+      tokenAssetValue,
+      fee
+    );
+    console.log('signer', signer, spendingSigner);
+    const controller = sdk.createDefiController(
+      accountPublicKey!,
+      spendingSigner!,
+      bridge,
+      tokenAssetValue,
+      fee
+    );
+    await controller.createProof();
+    const txId = await controller.send();
+    console.log(
+      'View transaction on the block explorer',
+      `tx/${txId.toString()}`
+    );
+  }
+
+  async function getBalance() {
+    let balance = sdk.fromBaseUnits(
+      await sdk.getBalance(accountPublicKey, sdk.getAssetIdBySymbol('ETH'))
+    );
+
+    let spendableAccountSum = sdk.fromBaseUnits({
+      assetId: 0,
+      value: await sdk.getSpendableSum(accountPublicKey, 0, false),
+    });
+
+    let spendableSpendingKeySum = sdk.fromBaseUnits({
+      assetId: 0,
+      value: await sdk.getSpendableSum(accountPublicKey, 0, true),
+    });
+
+    let pendingSpendingKeySum = sdk.fromBaseUnits({
+      assetId: 0,
+      value: await sdk.getSpendableSum(accountPublicKey, 0, true, false),
+    });
+
+    const padding = 50;
+
+    console.log(`Total zkETH Balance:`.padEnd(padding, ' '), balance);
+    console.log(
+      'Spendable base account zkETH Balance:'.padEnd(padding, ' '),
+      spendableAccountSum
+    );
+    console.log(
+      'Spendable registered account zkETH Balance:'.padEnd(padding, ' '),
+      spendableSpendingKeySum
+    );
+    console.log(
+      'Pending registered account zkETH Balance:'.padEnd(padding, ' '),
+      pendingSpendingKeySum
+    );
+
+    const defiTxs = await sdk.getDefiTxs(accountPublicKey);
+
+    console.log(defiTxs);
+
+    defiTxs.map(async (tx) => {
+      console.log(`Bridge Id: ${tx.bridgeCallData.bridgeAddressId}`);
+      const balance = await sdk.getBalance(
+        accountPublicKey,
+        tx.bridgeCallData.outputAssetIdA
+      );
+      console.log(
+        `Output asset id ${tx.bridgeCallData.outputAssetIdA} balance: ${balance.value}`
+      );
+      setVirtualAssetId(tx.bridgeCallData.outputAssetIdA);
+    });
+  }
+
+  async function mapVirtualAssetToNFT() {
+    const id = virtualAssetId;
+    const bridgeAddress = '0xa6c8113388ef842b2aca1d3ce41ecccbb4a35e2a';
+    const bridgeContract = new ethers.Contract(
+      bridgeAddress,
+      bridgeReadAbi,
+      signer
+    );
+    const nftId = await bridgeContract.owners(virtualAssetId);
+    console.log('NFT id', nftId.toNumber());
   }
 
   async function getSpendingKey() {
     const { privateKey } = await sdk!.generateSpendingKeyPair(ethAccount!);
     const signer = await sdk?.createSchnorrSigner(privateKey);
-    console.log("signer added", signer);
+    console.log('signer added', signer);
     setSpendingSigner(signer);
   }
 
-  async function registerNewAccount() {
-    const depositTokenQuantity: bigint = ethers.utils
-      .parseEther(amount.toString())
-      .toBigInt();
-    const recoverySigner = await sdk!.createSchnorrSigner(randomBytes(32));
-    let recoverPublicKey = recoverySigner.getPublicKey();
-    let txId = await registerAccount(
-      accountPublicKey!,
-      alias,
-      accountPrivateKey!,
-      spendingSigner!.getPublicKey(),
-      recoverPublicKey,
-      EthAddress.ZERO,
-      depositTokenQuantity,
-      TxSettlementTime.NEXT_ROLLUP,
-      ethAccount!,
-      sdk!
-    );
-    console.log("registration txId", txId);
-    console.log(
-      "lookup tx on explorer",
-      `https://aztec-connect-testnet-explorer.aztec.network/goerli/tx/${txId.toString()}`
-    );
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!paused) {
+        if (activeImg === 20) {
+          setActiveImg(1);
+          console.log('restart');
+        } else {
+          setActiveImg(activeImg + 1);
+          console.log('going');
+        }
+      }
+    }, intervalSpeed);
 
-  async function depositEth() {
-    const depositTokenQuantity: bigint = ethers.utils
-      .parseEther(amount.toString())
-      .toBigInt();
-
-    let txId = await depositEthToAztec(
-      ethAccount!,
-      accountPublicKey!,
-      depositTokenQuantity,
-      TxSettlementTime.NEXT_ROLLUP,
-      sdk!,
-    );
-
-    console.log("deposit txId", txId);
-    console.log(
-      "lookup tx on explorer",
-      `https://aztec-connect-testnet-explorer.aztec.network/goerli/tx/${txId.toString()}`
-    );
-  }
+    return () => clearInterval(interval);
+  }, [activeImg, paused]);
 
   return (
     <div>
-      {hasMetamask ? (
-        isConnected ? (
-          "Connected! "
-        ) : (
-          <button onClick={() => connect()}>Connect</button>
-        )
-      ) : (
-        "Please install metamask"
-      )}
-      {connecting ? "Please wait, setting up Aztec" : ""}
-      {sdk ? (
-        <div>
-          {accountPrivateKey ? (
-            <button onClick={() => initUsersAndPrintBalances()}>
-              Init User / Log Balance
-            </button>
-          ) : (
-            <button onClick={() => login()}>Login</button>
-          )}
-          {spendingSigner && !userExists ? (
-            <form>
-              <label>
-                Alias:
-                <input
-                  type="text"
-                  value={alias}
-                  onChange={(e) => setAlias(e.target.value)}
-                />
-              </label>
-            </form>
-          ) : (
-            ""
-          )}
-          {!spendingSigner && account0 ? (
-            <button onClick={() => getSpendingKey()}>
-              Create Spending Key (Signer)
-            </button>
-          ) : (
-            ""
-          )}
-          {spendingSigner ? (
-            <div>
-              <form>
-                <label>
-                  Deposit Amount:
-                  <input
-                    type="number"
-                    step="0.000000000000000001"
-                    min="0.01"
-                    value={amount}
-                    onChange={(e) => setAmount(e.target.valueAsNumber)}
-                  />
-                  ETH
-                </label>
-              </form>
-              {!userExists ? (
-                <button onClick={() => registerNewAccount()}>
-                  Register Alias + Deposit ≥0.1 ETH
-                </button>
+      <main>
+        {minted && <Confetti />}
+        <div className="custom-container">
+          <div className="wrapper">
+            <div className="content-wrapper">
+              {/* <h1>zkmint</h1> */}
+              <Image
+                src="/imgs/logo.png"
+                width={300}
+                height={200}
+                objectFit="contain"
+              />
+              <p>Private nft minting on Aztec</p>
+              {/* {hasMetamask ? (
+                isConnected ? (
+                  <div>
+                    {sdk ? (
+                      <div>
+                        {accountPrivateKey ? (
+                          <div>
+                            {account0 ? (
+                              <div>
+                                {spendingSigner ? (
+                                  <Button onClick={() => mint()}>Mint</Button>
+                                ) : (
+                                  <Button onClick={() => getSpendingKey()}>
+                                    Generate my spending key
+                                  </Button>
+                                )}
+                              </div>
+                            ) : (
+                              <Button
+                                onClick={() => initUsersAndPrintBalances()}
+                              >
+                                Init
+                              </Button>
+                            )}
+                          </div>
+                        ) : (
+                          <Button onClick={() => login()}>Log in</Button>
+                        )}
+                      </div>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                ) : (
+                  <Button onClick={() => connect()}>Connect to Aztec</Button>
+                )
               ) : (
-                ""
-              )}
+                'Please install metamask'
+              )} */}
+              <div>
+                {hasMetamask ? (
+                  isConnected ? (
+                    ''
+                  ) : (
+                    <Button onClick={() => connect()}>Connect to Aztec</Button>
+                  )
+                ) : (
+                  'Please install metamask'
+                )}
+                {connecting ? 'Please wait, setting up Aztec' : ''}
+                {sdk ? (
+                  <div>
+                    {accountPrivateKey && !account0 ? (
+                      <Button onClick={() => initUsersAndPrintBalances()}>
+                        Initialize
+                      </Button>
+                    ) : (
+                      ''
+                    )}
+                    {!accountPrivateKey ? (
+                      <Button onClick={() => login()}>Connect Wallet</Button>
+                    ) : (
+                      ''
+                    )}
+                    {spendingSigner && account0 ? (
+                      <>
+                        {minted ? (
+                          <Button>Export to L1</Button>
+                        ) : (
+                          <Button onClick={() => mint()}>Mint my NFT</Button>
+                        )}
+                        {/* <Button onClick={() => getBalance()}>
+                          Get Balance
+                        </Button> */}
+                      </>
+                    ) : (
+                      ''
+                    )}
+                    {spendingSigner && account0 && virtualAssetId ? (
+                      <Button onClick={() => mapVirtualAssetToNFT()}>
+                        retrieve NFT ID
+                      </Button>
+                    ) : (
+                      ''
+                    )}
+                    {spendingSigner && !userExists ? (
+                      <form>
+                        <label>
+                          Alias:
+                          <input
+                            type="text"
+                            value={alias}
+                            onChange={(e) => setAlias(e.target.value)}
+                          />
+                        </label>
+                      </form>
+                    ) : (
+                      ''
+                    )}
+                    {!spendingSigner && account0 ? (
+                      <Button onClick={() => getSpendingKey()}>
+                        Create My Spending Key
+                      </Button>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                ) : (
+                  ''
+                )}
+              </div>
             </div>
-          ) : (
-            ""
-          )}
-          {spendingSigner && account0 ? (
-            <button onClick={() => depositEth()}>Deposit ETH</button>
-          ) : (
-            ""
-          )}
-          <button onClick={() => console.log("sdk", sdk)}>Log SDK</button>
+            <div className="img-wrapper">
+              {minted ? (
+                <span className="amount">Your zkNFT</span>
+              ) : (
+                <span className="amount">{quanity} available</span>
+              )}
+
+              <div className="img-thing">
+                <Image
+                  src={`/imgs/${activeImg}.png`}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+              {!minted && <span className="price">Price: 0Ξ</span>}
+            </div>
+          </div>
         </div>
-      ) : (
-        ""
-      )}
+      </main>
+
+      {connecting ? 'Please wait, setting up Aztec' : ''}
     </div>
   );
 };
